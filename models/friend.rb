991aaeba066547cadf27c09f95808b20b4153f9d
Jonathan Walton <lemez9@gmail.com>
2019-06-07 11:58:14 +0200

    age and significant birthday methods

--- models/friend.rb
+++ models/friend.rb
@@ -16,6 +16,25 @@ class Friend < ActiveRecord::Base
       return  ((bday - today >-1) && (bday - today <= num))
   end
 
+  def age
+      bday = self.birthday.strftime('%j').to_i
+      today = Date.today.strftime('%j').to_i
+
+      if today >= bday
+        age = Date.today.year - self.birthday.year 
+      else
+        age = Date.today.year - self.birthday.year  - 1
+      end
+      age
+  end
+
+  def significant?
+    
+       @significant_birthdays = Array (1..18)
+       @significant_birthdays += [21,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
+      return @significant_birthdays.include?(self.age+1)
+  end
+
 
   def self.all_upcoming(num)
     Friend.all.select {|bday|bday.upcoming?(num)}
